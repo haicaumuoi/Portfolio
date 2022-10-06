ca901103e4d1e4809e186698f5f933cf2eeb43e1
Hải Anh <93531561+HaiCauMuoi@users.noreply.github.com>
2022-10-06 22:37:06 +0700

    Changes

---
+++ .next/server/pages/api/getPageInfo.js
@@ -0,0 +1,79 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 677;
+exports.ids = [677];
+exports.modules = {
+
+/***/ 5879:
+/***/ ((module) => {
+
+module.exports = require("next-sanity");
+
+/***/ }),
+
+/***/ 6753:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ handler)
+/* harmony export */ });
+/* harmony import */ var next_sanity__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5879);
+/* harmony import */ var next_sanity__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(next_sanity__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _sanity__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(9619);
+
+
+const query = next_sanity__WEBPACK_IMPORTED_MODULE_0__.groq`*[_type == "pageInfo"][0]`;
+async function handler(req, res) {
+    const pageInfo = await _sanity__WEBPACK_IMPORTED_MODULE_1__/* .sanityClient.fetch */ .i3.fetch(query);
+    res.status(200).json({
+        pageInfo
+    });
+}
+
+
+/***/ }),
+
+/***/ 9619:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "i3": () => (/* binding */ sanityClient)
+});
+
+// UNUSED EXPORTS: config, urlFor
+
+;// CONCATENATED MODULE: external "@sanity/client"
+const client_namespaceObject = require("@sanity/client");
+var client_default = /*#__PURE__*/__webpack_require__.n(client_namespaceObject);
+;// CONCATENATED MODULE: external "@sanity/image-url"
+const image_url_namespaceObject = require("@sanity/image-url");
+;// CONCATENATED MODULE: ./sanity.ts
+
+
+const config = {
+    dataset: "production" || 0,
+    projectId: "npy7f8jp",
+    apiVerson: "2021-03-25",
+    useCdn: "production" === "production"
+};
+const sanityClient = client_default()(config);
+const urlFor = (source)=>creatImageUrlBuilder(config).image(source);
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-api-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(6753));
+module.exports = __webpack_exports__;
+
+})();
