08de83ca96f957a4143e2576f17cf23cb859e735
Hải Anh <93531561+HaiCauMuoi@users.noreply.github.com>
2022-10-07 09:57:48 +0700

    add

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4a843add389cbb54"; }
+/******/ 		__webpack_require__.h = function() { return "617a677c67aa2807"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- sanity.ts
+++ sanity.ts
@@ -4,7 +4,7 @@ import creatImageUrlBuilder from '@sanity/image-url';
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  apiVerson: '2021-03-25',
+  apiVerson: '2022-10-05',
   useCdn: process.env.NODE_ENV === 'production',
 };
 
